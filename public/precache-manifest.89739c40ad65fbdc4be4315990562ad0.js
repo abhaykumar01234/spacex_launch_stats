640e1ab4934461cfed5d6a8ff56de5b69395ce6e
Abhay Kumar <abhaykumar01234@gmail.com>
2019-10-23 16:04:08 +0530

    Before deployment

---
+++ public/precache-manifest.89739c40ad65fbdc4be4315990562ad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58067c89c130d66fe9ecf855f050173a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f2794b434b3b622aded",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "48962d74e6fe68d0aff9",
+    "url": "/static/js/2.e9ef455b.chunk.js"
+  },
+  {
+    "revision": "7f2794b434b3b622aded",
+    "url": "/static/js/main.884d476c.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "2974197415ffdab9aa0504753613f1e4",
+    "url": "/static/media/spacex.29741974.jpg"
+  }
+]);
